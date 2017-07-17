9990ca367b08dbd77b9d57f6827754a0e87033a0
cmgiler <cmgiler@gmail.com>
2017-07-17 12:58:14 -0400

    Complete Section 5 of Udemy course ("Storing resources in a SQL database")

--- Section5-Storing_Resources_in_SQL_Database/app.py
+++ Section5-Storing_Resources_in_SQL_Database/app.py
@@ -1,9 +1,10 @@
 from flask import Flask
-from flask_jwt import JWT, jwt_required
-from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT
+from flask_restful import Api
 
 from supporting_code.security import authenticate, identity
 from supporting_code.user import UserRegister
+from supporting_code.item import Item, ItemList
 
 app = Flask(__name__)
 app.secret_key = 'chris'
@@ -11,52 +12,9 @@ api = Api(app)
 
 jwt = JWT(app, authenticate, identity) # /auth
 
-items = []
-
-class Item(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('price',
-                        type=float,
-                        required=True,
-                        help="This field cannot be left blank!")
-
-    @jwt_required()
-    def get(self, name):
-        item = next(filter(lambda x: x['name'] == name, items), None) # Gives first item found by filter function
-        return {'item': item}, 200 if item else 404
-
-    def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None) is not None:
-            return {'message': "An item with name '{}' already exists.".format(name)}, 400
-
-        data = Item.parser.parse_args()
-
-        item = {'name': name, 'price': data['price']}
-        items.append(item)
-        return item, 201
-
-    def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] != name, items))
-        return {'message': 'Item deleted'}
-
-    def put(self, name):
-        data = Item.parser.parse_args()
-
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        if item is None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
-        else:
-            item.update(data)
-        return item
-
-class ItemList(Resource):
-    def get(self):
-        return {'items': items}
-
 api.add_resource(Item, '/item/<string:name>') # http://127.0.0.1:5000/student/Chris
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
-app.run(port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
