fb5b077416e2cbdb6f1e9523c6b047fe81403113
cmgiler <cmgiler@gmail.com>
2017-07-17 22:24:27 -0400

    Complete Section 6 of Udemy course (SQLAlchemy).

--- Section6-Flask_SQLAlchemy/app.py
+++ Section6-Flask_SQLAlchemy/app.py
@@ -3,18 +3,30 @@ from flask_jwt import JWT
 from flask_restful import Api
 
 from resources.item import Item, ItemList
+from resources.store import Store, StoreList
 from resources.user import UserRegister
 from security import authenticate, identity
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # Turns off Flask SQLAlchemy tracker; Standard tracker still runs.
 app.secret_key = 'chris'
 api = Api(app)
 
+# Create table using SQLAlchemy / Flask decorator
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
 jwt = JWT(app, authenticate, identity) # /auth
 
+api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>') # http://127.0.0.1:5000/student/Chris
+api.add_resource(StoreList, '/stores')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
     app.run(port=5000, debug=True)
